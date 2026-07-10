ac050070eef251dcef9f73b9ebe4ff26f6cecc0a
Aradhya <26aradhyajha@gmail.com>
2026-07-10 15:37:33 +0530

    Handle weather search form submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Weather Dashboard Project Started");
+// Select HTML elements
+const form = document.getElementById("weather-form");
+const cityInput = document.getElementById("city");
+
+// Listen for form submit
+form.addEventListener("submit", function (event) {
+
+    // Prevent page refresh
+    event.preventDefault();
+
+    // Get input value
+    const city = cityInput.value.trim();
+
+    // Check if input is empty
+    if (city === "") {
+        alert("Please enter a city name.");
+        return;
+    }
+
+    // Print city name in console
+    console.log("City:", city);
+
+});
